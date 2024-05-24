e24fe2b4ee5e3793487635aa1130a0904bf97975
ava <148492613+avaceratops@users.noreply.github.com>
2024-05-24 11:58:14 +0100

    Update app.js
    - Add additional error logging using the debug package.
    - Enable mongoose connection.
    - Remove unnecessary CSP header settings.
    - Add routers.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
+const debug = require('debug')('inventory-app:app');
 const express = require('express');
-const helmet = require('helmet');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -10,31 +10,25 @@ const RateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
+const categoryRouter = require('./routes/category');
+const subcategoryRouter = require('./routes/subcategory');
+const productRouter = require('./routes/product');
 
 const app = express();
 app.set('trust proxy', 1); // needed for Railway hosting
 
 // set up mongoose connection
-// mongoose.set('strictQuery', false);
-// const mongoDB = process.env.MONGODB_URI;
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URI;
 
-// main().catch((err) => debug(err));
-// async function main() {
-//   await mongoose.connect(mongoDB);
-// }
+main().catch((err) => debug(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-// set CSP headers to allow our Bootstrap and Jquery to be served
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      'script-src': ["'self'", 'code.jquery.com', 'cdn.jsdelivr.net'],
-    },
-  })
-);
 
 // set up rate limiter to reduce bandwidth consumption, maximum of sixty requests per minute
 const limiter = RateLimit({
@@ -52,6 +46,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/category', categoryRouter);
+app.use('/subcategory', subcategoryRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -63,6 +60,8 @@ app.use((err, req, res, _next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  debug(err);
 
   // render the error page
   res.status(err.status || 500);
